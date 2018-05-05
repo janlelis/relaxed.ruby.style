41743f736fcc8b8f2cbcba679078f8a14b1efbf5
Jan Lelis <mail@janlelis.de>
2018-05-05 17:17:51 +0200

    Add header linking

---
+++ source/javascripts/patches.js
@@ -0,0 +1,17 @@
+$(function(){
+  $('h2').each(function(_, element){
+    if(!$(element).find('a').length){
+      $(element).replaceWith(
+        $('<h2 id="' + element.id + '"><a href="#' + element.id + '">' + $(element).html() + '</a></h2>')
+      )
+    }
+  });
+
+  $('h3').each(function(_, element){
+    if(!$(element).find('a').length){
+      $(element).replaceWith(
+        $('<h3 id="' + element.id + '"><a href="#' + element.id + '">' + $(element).html() + '</a></h3>')
+      )
+    }
+  });
+});
